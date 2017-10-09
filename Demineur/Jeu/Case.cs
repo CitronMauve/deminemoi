151ab078d3cad34844bcc8188e07b3c310984e49
Romain Gille <romain.gille@gmail.com>
2017-10-09 09:41:40 +0200

    Add new class Case

---
+++ Demineur/Jeu/Case.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur.Jeu
+{
+    class Case
+    {
+        private bool minee, marquee, decouverte;
+        private void Marquer()
+        {
+
+        }
+        private void Decouvrir()
+        {
+
+        }
+        private void Afficher()
+        {
+
+        }
+    }
+}
