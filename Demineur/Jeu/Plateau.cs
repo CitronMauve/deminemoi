531ad12084606350c2acafbba8f9d12bb4387f5a
nguyhuy <nguyhuy@pc5403s.lan.esiee.fr>
2017-10-09 09:39:16 +0200

    Add new class Plateau bitch

---
+++ Demineur/Jeu/Plateau.cs
@@ -0,0 +1,45 @@
+﻿using Jeu;
+
+namespace Demineur
+{
+    class Plateau
+    {
+        public Partie partie;
+
+        public int largeur;
+        public int hauteur;
+        Case[,] cases;
+
+        private int mines;
+        private int decouvertes;
+        private int restantes;
+
+        public Case Trouver(int x, int y)
+        {
+            return cases[x, y];
+        }
+
+        public void IncrementerDecouvertes()
+        {
+            decouvertes++;
+        }
+
+        public void ModifierMarquees(bool marquee)
+        {
+            if (marquee)
+            {
+                restantes--;
+            } else
+            {
+                restantes++;
+            }
+
+            partie.vue.ActualiserComptage(restantes);
+        }
+
+        public bool TesterSiGagne()
+        {
+            return decouvertes + mines == largeur * hauteur;
+        }
+    }
+}
